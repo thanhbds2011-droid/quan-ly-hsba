29cec4f09a52b0ee407f143f925aa83c563390a6
thanhbds2011-droid <thanhbds2011@gmail.com>
2026-07-10 20:50:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,16 @@
+window.HSBA_CONFIG = Object.freeze({
+  // Dán URL Web App kết thúc bằng /exec vào đây.
+  // Ví dụ: https://script.google.com/macros/s/AKfycb.../exec
+  API_URL: 'DAN_URL_APPS_SCRIPT_WEB_APP_VAO_DAY',
 
+  APP_NAME: 'Quản lý HSBA Trung tâm',
+  VERSION: '1.0.0',
+
+  STATUS: {
+    DANG_KHAM: 'Đang khám',
+    DA_LUU_KHO: 'Đã lưu kho',
+    TU_VONG: 'Đối tượng tử vong'
+  },
+
+  MAX_UPLOAD_BYTES: 10 * 1024 * 1024
+});
